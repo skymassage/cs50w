e607b74876dac74cb61d0338761bec0275e0a93f
lin <a6491080230@gmail.com>
2024-04-22 15:58:01 +0800

    Update

--- selenium/p.py
+++ selenium/p.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
 
 # print(Service("./chromedriver.exe").path)
 
@@ -7,4 +9,6 @@ from selenium.webdriver.chrome.service import Service
 driver = webdriver.Chrome()
 
 # s = Service("./chromedriver.exe")
+# s = Service("/usr/bin/chromedriver")
+
 # driver = webdriver.Chrome(service=s)
